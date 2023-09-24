4764bc3ce96d54abc076022e591d1fd4b361a3df
Fabricio Floriano <f.florian0@hotmail.com>
2023-09-24 17:53:09 -0300

    Req 6: Criando Users

---
+++ app/backend/src/database/models/UsersModel.ts
@@ -0,0 +1,49 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class UsersModel extends Model<InferAttributes<UsersModel>,
+InferCreationAttributes<UsersModel>> {
+  declare id: CreationOptional<number>;
+  declare username: string;
+  declare password: string;
+  declare role: string;
+  declare email: string;
+}
+
+UsersModel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+}, {
+  sequelize: db,
+  underscored: true,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default UsersModel;
